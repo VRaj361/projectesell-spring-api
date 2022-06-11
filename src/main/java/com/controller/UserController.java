1262dd4b765ff6fed833d1e9c308d19d6d6317e2
Vraj <vrajnpatel16@gmail.com>
2022-06-11 15:29:02 +0530

    Modified Controller

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -33,7 +33,7 @@ public class UserController {
 	}
 	
 	@PutMapping("/user")
-	public UserBean updateUser(  UserBean user) {
+	public UserBean updateUser(@RequestBody UserBean user) {
 		userDao.updateUser(user);
 		return user;
 	}
